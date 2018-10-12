e351db71df83f9353acab33e46392fcc97237012
Marious Davis <mlancelot88@gmail.com>
2018-10-12 13:05:04 -0500

    POST request handling added for Actions

--- data/routes/action/index.js
+++ data/routes/action/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const actionModel = require("../../data/helpers/actionModel.js");
+const projectModel = require("../../data/helpers/projectModel.js");
 const router = express.Router();
 
 // Retrieves all available actions
@@ -27,9 +28,19 @@ router.get("/:id", (req, res) => {
       res
         .status(500)
         .json(
-          `Server could not retrieve action information: ${err}. It is likely the action with ID ${id} does not exist.`
+          `Server could not retrieve information: ${err}. Action, ID ${id}, does not exist.`
         )
     );
+});
+
+// Allow user to post new action. Return action submission.
+router.post("/", (req, res) => {
+  const { project_id, description, notes } = req.body;
+  const newAction = { project_id, description, notes };
+  actionModel
+    .insert(newAction)
+    .then(action => res.status(201).json(action))
+    .catch(err => res.status(500).json(`Unable to post new action: ${err}`));
 });
 
 module.exports = router;
