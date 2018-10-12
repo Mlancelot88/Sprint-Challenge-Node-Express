f2f8939bc32351d47cbcc4cc0aa998af393e9347
Marious Davis <mlancelot88@gmail.com>
2018-10-12 12:47:09 -0500

    updated index.js to handle incoming action routes

--- data/index.js
+++ data/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const applyGlobalMiddleware = require("./middleware/globalMiddleware.js");
-const projectRoutes = require("./routes/project");
+const { projectRoutes, actionRoutes } = require("./routes");
 
 const server = express();
 const port = 3300;
@@ -8,4 +8,6 @@ const port = 3300;
 applyGlobalMiddleware(server);
 
 server.use("/api/projects", projectRoutes);
+server.use("/api/actions", actionRoutes);
+
 server.listen(port, () => console.log(`Server is running on port ${port}`));
